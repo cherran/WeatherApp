e3956aa90e99d151b069db844a1a24a889e8bd02
Carlos Herrán <cherranm@gmail.com>
2018-05-09 18:01:43 +0200

    hasta LinearLayout

--- app/src/main/java/dev/cherran/weatherapp/MainActivity.kt
+++ app/src/main/java/dev/cherran/weatherapp/MainActivity.kt
@@ -2,6 +2,7 @@ package dev.cherran.weatherapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +12,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var number = savedInstanceState?.getInt("Number")
 
         Log.v("aTag", "Han llamado a OnCreate V")
         Log.d("aTag", "Han llamado a OnCreate")
         Log.e("aTag", "Han llamado a onCreate")
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+
+        // Para guardar datos antes de la destrucción de la vista en el outState (Diccionario)
+        outState?.putInt("Number", 3)
     }
 }
