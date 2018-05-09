19a3a6d71aacb8c6ce1852619937252401ab5373
Carlos Herrán <cherranm@gmail.com>
2018-05-09 13:01:48 +0200

    Testing Logs

--- app/src/main/java/dev/cherran/weatherapp/MainActivity.kt
+++ app/src/main/java/dev/cherran/weatherapp/MainActivity.kt
@@ -2,11 +2,18 @@ package dev.cherran.weatherapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+// En Kotlin al heredar de una clase se pone el constructor de la superclase por defecto (En este caso sin argumento)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) { // Bundle? -> Optional
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Log.v("aTag", "Han llamado a OnCreate V")
+        Log.d("aTag", "Han llamado a OnCreate")
+        Log.e("aTag", "Han llamado a onCreate")
     }
 }
