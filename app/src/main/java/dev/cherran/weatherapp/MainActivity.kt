f457c8c5f0f1904828e9b2b77d1447f9a5987cb2
Carlos Herrán <cherranm@gmail.com>
2018-05-10 20:30:34 +0200

    Buttons in MainActivity change the ImageView between two images

--- app/src/main/java/dev/cherran/weatherapp/MainActivity.kt
+++ app/src/main/java/dev/cherran/weatherapp/MainActivity.kt
@@ -5,15 +5,17 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 // En Kotlin al heredar de una clase se pone el constructor de la superclase por defecto (En este caso sin argumento)
 
     val TAG = MainActivity::class.java.canonicalName // Buena práctica para establecer el TAG de los logs de cada clase
+    var forecastImage: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) { // Bundle? -> Optional
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) // Hasta aquí no puedo acceder a los elementos de la vista
 
         // var number = savedInstanceState?.getInt("Number")
 
@@ -25,6 +27,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         // val europeanButton: Button = findViewById(R.id.european_system_button)
 
         val americanButton = findViewById(R.id.american_system_button) as? Button
+
+        forecastImage = findViewById(R.id.forecast_image)
 
         europeanButton?.setOnClickListener(this)
         americanButton?.setOnClickListener(this)
@@ -65,13 +69,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 //        }
 
        // Más PRO
-       Log.v(TAG, when (v?.id) { // En Kotlin no hay Switch statement, han When
-           R.id.european_system_button -> "Han pulsado el botón europeo"
-           R.id.american_system_button -> "Han pulsado el botón americano"
-           else -> "No tengo ni idea de qué es lo que han pulsado"
-       })
+//       Log.v(TAG, when (v?.id) { // En Kotlin no hay Switch statement, han When
+//           R.id.european_system_button -> "Han pulsado el botón europeo"
+//           R.id.american_system_button -> "Han pulsado el botón americano"
+//           else -> "No tengo ni idea de qué es lo que han pulsado"
+//       })
 
+        val imageToShow = when (v?.id) {
+            R.id.european_system_button -> R.drawable.offline_weather
+            else -> R.drawable.offline_weather2
+        }
 
+        forecastImage?.setImageResource(imageToShow) // Solo se llama al método si forecastImage != null
 
     }
 }
