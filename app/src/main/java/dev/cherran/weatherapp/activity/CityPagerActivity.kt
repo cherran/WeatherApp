c8b19808e683a73d08751729e7c5401d6809dbb7
Carlos Herrán <cherranm@gmail.com>
2018-05-31 21:59:31 +0200

    Added menu to CityPagerActivity

--- app/src/main/java/dev/cherran/weatherapp/activity/CityPagerActivity.kt
+++ app/src/main/java/dev/cherran/weatherapp/activity/CityPagerActivity.kt
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.PagerAdapter
+import android.view.Menu
+import android.view.MenuItem
 import dev.cherran.weatherapp.R
 import dev.cherran.weatherapp.fragment.ForecastFragment
 import dev.cherran.weatherapp.model.Cities
@@ -35,5 +38,40 @@ class CityPagerActivity : AppCompatActivity() {
         }
 
         view_pager.adapter = adapter
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.pager_navigation, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean = when(item?.itemId) {
+        R.id.previous -> {
+            view_pager.currentItem = view_pager.currentItem - 1
+            true
+        }
+        R.id.next -> {
+            view_pager.currentItem = view_pager.currentItem + 1
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
+
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        // Se llama cada vez que cambiamos de página
+        super.onPrepareOptionsMenu(menu)
+
+        val previousMenu: MenuItem? = menu?.findItem(R.id.previous)
+        val nextMenu: MenuItem? = menu?.findItem(R.id.next)
+
+        val adapter: PagerAdapter = view_pager.adapter!!
+        previousMenu?.isEnabled = view_pager.currentItem > 0
+        nextMenu?.isEnabled = view_pager.currentItem < adapter.count - 1
+
+        return true
     }
 }
